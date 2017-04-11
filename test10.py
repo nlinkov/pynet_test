f29d6d3a4b754d9db9358741e840decd3ec22c88
Nikolay Linkov <nikilinkov@gmail.com>
2017-04-11 07:22:46 -0700

    changes to test9 and excercise 10

--- test10.py
+++ test10.py
@@ -2,19 +2,26 @@
 
 from ciscoconfparse import CiscoConfParse
 from pprint import pprint as pp
+import re
 
 config_dump = CiscoConfParse('cisco_config.txt')
 
-ff_objects = config_dump.find_objects(r'pfs group2')
+ff_objects = config_dump.find_objects_wo_child(r'crypto map CRYPTO', 'AES')
 
 for i in ff_objects:
    if (i.is_child):
       print i.parent.text
       for j in i.parent.all_children:
-         print j.text
+         if (re.search('transform-set', j.text) != None): 
+            print j.text
+         else:
+            pass
    elif (i.is_parent):
       print i.text
       for j in i.all_children:
-         print j.text
+        if (re.search('transform-set', j.text) != None):
+            print j.text
+        else:
+             pass
    else:
       pass
